dca1a99c34b8cbeb022705291b0bf4303a4ba466
Marcos Ganine <mganine@thoughtworks.com>
2015-09-16 15:35:32 -0300

    Adiciona script com configuração inicial do banco @marcosx & @ganine

---
+++ bootstrap.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE 'phonebook';
+CREATE USER 'phonebookuser'@'localhost' IDENTIFIED BY 'phonebookpassword';
+GRANT ALL ON phonebook.* TO 'phonebookuser'@'localhost';
+USE phonebook;
+CREATE TABLE IF NOT EXISTS `contact` ( `id` int(11) NOT NULL AUTO_INCREMENT, `firstName` varchar(255) NOT NULL, `lastName` varchar(255) NOT NULL, `phone` varchar(30) NOT NULL, PRIMARY KEY (`id`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1; 
+
